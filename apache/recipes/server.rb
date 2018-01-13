e0cacf58711f345a0cec88518cd0bbf64c49cd38
Vishal Shah <vish2398@gmail.com>
2018-01-13 09:36:45 -0500

    adding ip address and hostname from ohai to webpage

--- apache/recipes/server.rb
+++ apache/recipes/server.rb
@@ -3,7 +3,11 @@ package 'httpd' do
 end
 
 file '/var/www/html/index.html' do
- content '<h1>HELLO WORLD FROM CHEF!</h1>'
+ content "
+ <h1>HELLO WORLD FROM CHEF!</h1>
+ <h2>ipaddress: #{node['ec2']['public_ipv4']}</h2>
+ <h2>hostname: #{node['ec2']['public_hostname']}</h2>
+ "
 end
 
 service 'httpd' do
--- workstation/recipes/setup.rb
+++ workstation/recipes/setup.rb
@@ -11,7 +11,12 @@ package 'git' do
 end
 
 file '/etc/motd' do
-  content 'This server is the proprty of Vishal Shah'
+  content "This server is the proprty of Vishal Shah
+  HOSTNAME: #{node['hostname']}
+  IPADDRESS: #{node['ipaddress']} 
+  CPU: #{node['cpu']['0']['mhz']}
+  MEMORY: #{node['memory']['total']}
+  "
   owner 'root'
   group 'root'
   action :create
