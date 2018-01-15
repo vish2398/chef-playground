28363df08525b4278018f9b781776aeaa117c564
Vishal Shah <vish2398@gmail.com>
2018-01-15 12:26:15 -0500

    refactored motd file to use templates

--- workstation/recipes/setup.rb
+++ workstation/recipes/setup.rb
@@ -10,15 +10,8 @@ package 'git' do
   action:install
 end
 
-file '/etc/motd' do
-  content "This server is the proprty of Vishal Shah
-  HOSTNAME: #{node['hostname']}
-  IPADDRESS: #{node['ipaddress']} 
-  CPU: #{node['cpu']['0']['mhz']}
-  MEMORY: #{node['memory']['total']}
-  "
-  owner 'root'
-  group 'root'
+template '/etc/motd' do
+  source 'motd.erb'
   action :create
 end
 
